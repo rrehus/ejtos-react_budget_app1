f55ced9dcc6afd2a4e2107de227285c42f8036d4
rrehus <rrehus98@gmail.com>
2024-06-11 08:55:59 -0400

    Budget allocation task

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,13 +3,26 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const { expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const value = event.target.value;
+        if (value>20000) {
+            alert("This value cannot exceed £20,000");
+            setNewBudget("");
+        }
+        else if (value<{expenses})
+        {
+            alert("This value cannot be less than the spending");
+            setNewBudget("");
+        }
+        else {
+            setNewBudget(event.target.value);
+        }
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
